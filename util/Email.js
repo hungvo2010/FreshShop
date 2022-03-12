beb4728b82234cb95bb6519a608f43d7837b3162
hungvochanhdp1 <hungvochanhdp1@gmail.com>
2022-03-12 11:40:08 +0700

    Add email delivery provider

--- util/Email.js
+++ util/Email.js
@@ -1,81 +1,79 @@
 const nodemailer = require('nodemailer');
+const nodemailerSendgrid = require('nodemailer-sendgrid');
 const { google } = require("googleapis");
 const path = require('path');
 const OAuth2 = google.auth.OAuth2;
+const ejs = require('ejs');
 
 require('dotenv').config({path: path.join(__dirname, '.env')});
 
 class Email {
-    constructor(email){
-        this.to = email;
-        this.from = process.env.USER_EMAIL
+    constructor(){
+        this.dev = process.env.NODE_ENV !== 'production';
+        this.from = dev ? process.env.SENDER_EMAIL_TEST : process.env.SENDER_EMAIL_LIVE;
     }
 
     createTransport(){
-        // if (process.env.NODE_ENV === 'development') {
-        //     return nodemailer.createTransport({
-        //       host: process.env.EMAIL_HOST,
-        //       port: process.env.EMAIL_PORT,
-        //       secure: false,
-        //       auth: {
-        //         user: process.env.EMAIL_USERNAME,
-        //         pass: process.env.EMAIL_PASSWORD
-        //       }
-        //     });
-        //   }
-        // if (process.env.NODE_ENV === 'production') {
-        //     //Sendgrid
-        //     return nodemailer.createTransport({
-        //         service: 'SendGrid',
-        //         auth: {
-        //         user: process.env.SENDGRID_USERNAME,
-        //         pass: process.env.SENDGRID_PASSWORD
-        //         }
-        //     });
-        // }
-        const oauth2Client = new OAuth2(
-            process.env.CLIENT_ID, // ClientID
-            process.env.CLIENT_SECRET, // Client Secret
-            "https://developers.google.com/oauthplayground" // Redirect URL
-        );
-        oauth2Client.setCredentials({
-            refresh_token: process.env.REFRESH_TOKEN
-        });
-        const accessToken = oauth2Client.getAccessToken();
-        return nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            auth: {
-                type: "OAuth2",
-                user: process.env.USER_EMAIL, 
-                clientId: process.env.CLIENT_ID,
-                clientSecret: process.env.CLIENT_SECRET,
-                refreshToken: process.env.REFRESH_TOKEN,
-                accessToken: accessToken
-            },
-            tls: {
-                rejectUnauthorized: false
-            }
-        });
-    }
+        if (this.dev){ // development or test
+            const oauth2Client = new OAuth2(
+                process.env.CLIENT_ID, // ClientID
+                process.env.CLIENT_SECRET, // Client Secret
+                "https://developers.google.com/oauthplayground" // Redirect URL
+            );
+            oauth2Client.setCredentials({
+                refresh_token: process.env.REFRESH_TOKEN
+            });
+            return nodemailer.createTransport({
+                host: 'smtp.gmail.com',
+                port: 465,
+                secure: true,
+                auth: {
+                    type: "OAuth2",
+                    user: this.from, 
+                    clientId: process.env.CLIENT_ID,
+                    clientSecret: process.env.CLIENT_SECRET,
+                    refreshToken: process.env.REFRESH_TOKEN,
+                },
+                tls: {
+                    rejectUnauthorized: false
+                }
+            });
+        }
 
-    async send(templateHtml, subject){
+        else {
+            return nodemailer.createTransport(
+                nodemailerSendgrid({
+                    apiKey: process.env.SENDGRID_API_KEY
+                })
+            );
+        }
+    }
+    
+    async send(...args){
+        const [template, subject, mailTo] = args;
+        const html = await ejs.renderFile(path.join(__dirname, "../views/emails", `${template}.pug`));
         const mailOptions = {
             from: this.from,
-            to: this.to,
+            to: mailTo,
             subject,
-            html: templateHtml,
+            html,
         };
         await this.createTransport().sendMail(mailOptions);
     }
 
-    async sendPasswordReset(templateHtml) {
+    async sendPasswordReset() {
         await this.send(
-           templateHtml,
+           'reset',
           'Your password reset token (valid for only 10 minutes)'
         );
-      }
+    }
+
+    async sendEmailWelcome() {
+        await this.send(
+           'welcome',
+          'Welcome to FreshShop'
+        );
+    }
 }
 
 module.exports = Email;
