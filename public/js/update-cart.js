c5d95c3ec0ad537081820c3c3444abc8563596a4
hungvochanhdp1 <hungvochanhdp1@gmail.com>
2022-03-17 23:07:08 +0700

    already find ways to update multiple records in a query

--- public/js/update-cart.js
+++ public/js/update-cart.js
@@ -1,7 +1,28 @@
 const updateCartButton = document.querySelector("#update-cart");
+const products = document.querySelectorAll("a[data-productId]");
+const productsQuantities = document.querySelectorAll("input[type='number']");
+
+function addItemsToProductsInfo(products, productsQuantities){
+    let productsInfo = [];
+    for (let i = 0; i < products.length; ++i){
+        productsInfo.push({productId: products[i].dataset.productid, quantity: +productsQuantities[i].value});
+    }
+    return productsInfo;
+}
 
 updateCartButton.addEventListener('click', event => {
-    const tbodyTable = document.querySelector('tbody');
-    const trow = tbodyTable.querySelectorAll('tr');;
+    event.preventDefault();
+    let productsInfo = addItemsToProductsInfo(products, productsQuantities);
+
+    fetch('http://localhost:3000/update-cart', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(productsInfo),
+    })
+    .then(res => {
+        
+    })
     
 })
