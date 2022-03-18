d6aa7bad78633bddf76757baff93d17098948e8c
hungvochanhdp1 <hungvochanhdp1@gmail.com>
2022-03-18 11:06:43 +0700

    explore a table for coupon or discount

--- public/js/update-cart.js
+++ public/js/update-cart.js
@@ -1,6 +1,7 @@
 const updateCartButton = document.querySelector("#update-cart");
 const products = document.querySelectorAll("a[data-productId]");
 const productsQuantities = document.querySelectorAll("input[type='number']");
+const productsPrices = document.querySelectorAll("p[data-price]");
 
 function addItemsToProductsInfo(products, productsQuantities){
     let productsInfo = [];
@@ -8,6 +9,30 @@ function addItemsToProductsInfo(products, productsQuantities){
         productsInfo.push({productId: products[i].dataset.productid, quantity: +productsQuantities[i].value});
     }
     return productsInfo;
+}
+
+function calculateSubTotal(productsPrices, productsQuantities){
+    let result = 0;
+    for (let index = 0; index < productsPrices.length; index++) {
+        result += +productsPrices[index].dataset.price * +productsQuantities[index].value;
+    }
+    return result;
+}
+
+function calculateDiscount(productsPrices, productsQuantities){
+    
+}
+
+function getCouponDiscount(){
+
+}
+
+function getVAT(){
+
+}
+
+function sentGrandTotal(grandTotal){
+
 }
 
 updateCartButton.addEventListener('click', event => {
@@ -22,7 +47,15 @@ updateCartButton.addEventListener('click', event => {
         body: JSON.stringify(productsInfo),
     })
     .then(res => {
-        
+        const statusCode = res.status.toString();
+        if (statusCode.startsWith('2')){
+            let subTotal = calculateSubTotal(productsPrices, productsQuantities);
+            let discount = calculateDiscount(productsPrices, productsQuantities);
+            let couponDiscount = getCouponDiscount();
+            let vat = getVAT();
+            let grandTotal = subTotal + discount + couponDiscount + vat;
+            sentGrandTotal(grandTotal);
+        }
     })
     
 })
